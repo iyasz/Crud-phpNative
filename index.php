5dc646406fb7cc937b3a804f30f42f4fec3c20d0
Yasz Avellia <zakamaragames@gmail.com>
2022-09-02 20:58:43 +0700

    menambah form

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="header">
+                                <h1>Formulir Pendaftaran</h1>
+                            </div>
+                            <div class="input">
+                                <label for="">Nama Lengkap</label>
+                                <input type="text" class="form-control" name="namalengkap">
+    
+                                <label for="">Nomor Induk Siswa</label>
+                                <input type="text" class="form-control" name="namalengkap">
+    
+                                <label for="">Nama Lengkap</label>
+                                <input type="text" class="form-control" name="namalengkap">
+    
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                    <label class="form-check-label" for="flexRadioDefault1">
+                                        Laki Laki
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                                    <label class="form-check-label" for="flexRadioDefault2">
+                                        Perempuan
+                                    </label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
